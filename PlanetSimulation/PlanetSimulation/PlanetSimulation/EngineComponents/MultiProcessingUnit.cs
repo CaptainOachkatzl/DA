7fdb2cbf4308e11b1f0f3464bef1419eb8b39f70
CaptainOachkatzl <dave.hofer@hotmail.com>
2017-08-01 12:55:24 +0200

    Project copy

---
+++ PlanetSimulation/PlanetSimulation/PlanetSimulation/EngineComponents/MultiProcessingUnit.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using PlanetSimulation.PhysicHandler;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Management;
+using XSLibrary.MultithreadingPatterns.UniquePair;
+using XSLibrary.MultithreadingPatterns.UniquePair.DistributionNodes;
+
+namespace PlanetSimulation.EngineComponents
+{
+    public class MultiProcessingUnit
+    {
+        private GravityHandling GravityHandler { get; set; }
+        private CollisionHandling CollisionHandler { get; set; }
+
+        private UniquePairDistribution<Planet, GameTime> m_pairDistribution;
+        private SharedMemoryPool<Planet, GameTime> m_distributionPool;
+
+        public int CoreCount { get; private set; }
+
+        int m_phyisicalCoreCount = 0;
+        public int PhysicalCoreCount
+        {
+            get
+            {
+                if (m_phyisicalCoreCount < 1)
+                    m_phyisicalCoreCount = GetPhysicalCoreCount();
+
+                return m_phyisicalCoreCount;
+            }
+        }
+
+        public int LogicalCoreCount { get { return Environment.ProcessorCount; } }
+
+        public MultiProcessingUnit(PlanetSim parent)
+        {
+            CoreCount = GetCoreCount();
+            m_distributionPool = new ActorPool<Planet, GameTime>(CoreCount, false);
+            m_pairDistribution = new RoundRobinTournamentDistribution<Planet, GameTime>(m_distributionPool);
+
+            GravityHandler = parent.GravityHandler;
+            CollisionHandler = parent.CollisionHandler;
+        }
+        public void Close()
+        {
+            m_pairDistribution.Dispose();
+        }
+
+        public void CalculatePlanetMovement(List<Planet> allPlanets, GameTime currentGameTime)
+        {
+            m_distributionPool.SetCalculationFunction(GravityHandler.CalculateGravity);
+            m_pairDistribution.Calculate(allPlanets.ToArray(), currentGameTime);
+            m_distributionPool.SetCalculationFunction(CollisionHandler.CalculateCollision);
+            m_pairDistribution.Calculate(allPlanets.ToArray(), currentGameTime);
+        }
+
+        private int GetCoreCount()
+        {
+            if (GameGlobals.MaximumProcessorsUsed > 0)
+                return GameGlobals.MaximumProcessorsUsed;
+
+            return LogicalCoreCount;
+        }
+
+        private int GetLogicalCoreCount()
+        {
+            return Environment.ProcessorCount;
+        }
+
+        private int GetPhysicalCoreCount()
+        {
+            int coreCount = 0;
+            foreach (ManagementBaseObject item in new ManagementObjectSearcher("Select NumberOfCores from Win32_Processor").Get())
+            {
+                coreCount += int.Parse(item["NumberOfCores"].ToString());
+            }
+            return coreCount;
+        }
+    }
+}
---
+++ XSLibrary/XSLibrary/XSMultithreadingPatterns/UniquePair/CalculationPair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace XSLibrary.MultithreadingPatterns.UniquePair
+{
+    public class CalculationPair<PartType, GlobalDataType> : IDisposable
+    {
+        public PartType[] Stack1 { get; set; }
+        public PartType[] Stack2 { get; set; }
+        public GlobalDataType GlobalData { get; set; }
+
+        public bool CalculateInternally { get; set; }
+
+        public CalculationPair(PartType[] part1, PartType[] part2, GlobalDataType globalData, bool calculateIntern)
+        {
+            Stack1 = part1;
+            Stack2 = part2;
+            GlobalData = globalData;
+
+            CalculateInternally = calculateIntern;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Stack1 = null;
+                Stack2 = null;
+            }
+        }
+    }
+}
