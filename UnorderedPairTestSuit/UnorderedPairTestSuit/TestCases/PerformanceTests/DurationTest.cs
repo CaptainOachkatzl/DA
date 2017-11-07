97e76666027ca97ca99909131d1da7ed71efe59c
CaptainOachkatzl <dave.hofer@hotmail.com>
2017-11-07 09:55:10 +0100

    adjusted sleep timer to be more accurate

--- UnorderedPairTestSuit/UnorderedPairTestSuit/TestCases/PerformanceTests/DurationTest.cs
+++ UnorderedPairTestSuit/UnorderedPairTestSuit/TestCases/PerformanceTests/DurationTest.cs
@@ -5,15 +5,17 @@ namespace UnorderedPairTestSuit
     abstract class DurationTest : TimeMeasurementTest
     {
         protected abstract int Duration { get; }
+        ManualResetEvent m_resetEvent;
 
         public DurationTest(int dummyCount) : base(dummyCount)
         {
+            m_resetEvent = new ManualResetEvent(false);
         }
 
         protected override void CalculationFunction(int element1, int element2, int global)
         {
             // fixed duration of the execution
-            Thread.Sleep(Duration);
+            m_resetEvent.WaitOne(Duration);
         }
     }
 }
